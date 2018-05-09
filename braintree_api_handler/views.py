4e271ea30b9605cda15da504d38a8577737b7b8c
ghovat <georg@sattler.io>
2018-05-09 17:44:31 +0200

    finished validate credentials fucntion

--- braintree_api_handler/views.py
+++ braintree_api_handler/views.py
@@ -26,6 +26,8 @@ def validate_credentials():
                         "message": "please submit a valid json body",
                         "transaction_id": transaction_id}), 400
 
+    data = request.get_json()
+
     if not "merchant_id" in data or not "environment" in data or not "public_key" in data or not "private_key" in data:
         app.logger.info("{}: the post request has missing fields".format(transaction_id))
         return jsonify({"status": "ERROR",
@@ -33,17 +35,52 @@ def validate_credentials():
                         "message": "you have to submit merchant_id, environment, public_key and private key",
                         "transaction_id": transaction_id}), 400
 
-    gateway = braintree.BraintreeGateway(
-        braintree.Configuration(
-            environment=braintree.Environment.Sandbox,
-            merchant_id="use_your_merchant_id",
-            public_key="use_your_public_key",
-            private_key="use_your_private_key"
+    if data["environment"] not in ["production", "sandbox"]:
+        app.logger.info("{}: no valid environment submitted, going to abort transaction".format(transaction_id))
+        return jsonify(
+            status="ERROR",
+            status_code=400,
+            message="environment must be either production or sandbox",
+            transaction_id=transaction_id
         )
-    )
 
-    client_token = gateway.client_token.generate({
-        "customer_id": "demo"
-    })
+    try:
+        if data["environment"] == 'production':
+            environment = braintree.Environment.Production
+        else:
+            environment = braintree.Environment.Sandbox
+        gateway = braintree.BraintreeGateway(
+            braintree.Configuration(
+                environment=environment,
+                merchant_id=data["merchant_id"],
+                public_key=data["public_key"],
+                private_key=data["private_key"]
+            )
+        )
+
+        try:
+            client_token = gateway.client_token.generate({})
+
+            app.logger.info("{}: successfully validated credentials: {}".format(transaction_id, client_token))
+            return jsonify(status="INFO",
+                           status_code=200,
+                           message="successfully validated credentials",
+                           token=client_token,
+                           transaction_id=transaction_id)
+        except Exception as e:
+            app.logger.info(e)
+            app.logger.info("{}: error while trying to retrieve token".format(transaction_id))
+            return jsonify(status="ERROR",
+                           status_code=401,
+                           message="not possible to authenticate user",
+                           transaction_id=transaction_id), 401
+    except Exception as e:
+        app.logger.info(e)
+        app.logger.info("{}: error while trying to generate token".format(transaction_id))
+        return jsonify(status="ERROR",
+                       status_code=500,
+                       message="not possible to authenticate user",
+                       transaction_id=transaction_id), 500
+
 
     return "d"
